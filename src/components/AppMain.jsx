0c65316926f89ec697b111b99dd4e5f46e82844f
martinasartori97 <martinasartori9@gmail.com>
2024-12-01 14:52:35 +0100

    edit appmain

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -2,8 +2,21 @@
 import ReactCountryFlag from 'react-country-flag';
 import { useGlobalContext } from "../context/GlobalContext"
 
+const languageToCountryCode = {
+    en: 'US',
+    it: 'IT',
+    fr: 'FR',
+    de: 'DE',
+    es: 'ES',
+
+};
+
+
+
+
 export default function AppMain() {
     const { movies } = useGlobalContext()
+
 
 
     return (
@@ -14,6 +27,7 @@ export default function AppMain() {
                     <p>titolo:{movie.title}</p>
                     <p>titolo originale:{movie.original_title}</p>
                     <p>language:{movie.original_language}</p>
+                    <ReactCountryFlag countryCode={movie.languageToCountryCode} />
                     <p>vote_average:{movie.vote_average}</p>
                 </div>
             ))}
